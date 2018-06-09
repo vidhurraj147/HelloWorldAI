b8f3a0994a892222fa3348e0517e77460d5c9805
vidhurraj147 <37847430+vidhurraj147@users.noreply.github.com>
2018-06-08 21:53:46 -0400

    Adding all files and JAR file

---
+++ HelloWorldAI/src/main/java/HelloWorldGI/HelloWorldAI/HellowWorldDemo.java
@@ -0,0 +1,12 @@
+package HelloWorldGI.HelloWorldAI;
+
+public class HellowWorldDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("THIS IS HelloWorldDemo MAVEN EXAMPLE");
+		System.out.println("ANOTHER LINE ADDED");
+
+	}
+
+}
