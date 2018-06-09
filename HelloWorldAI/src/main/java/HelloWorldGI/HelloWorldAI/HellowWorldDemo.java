099966495d722aad63d961cbfa8355d1c868457c
vidhurraj147 <37847430+vidhurraj147@users.noreply.github.com>
2018-06-08 22:36:40 -0400

    Added another method

--- HelloWorldAI/src/main/java/HelloWorldGI/HelloWorldAI/HellowWorldDemo.java
+++ HelloWorldAI/src/main/java/HelloWorldGI/HelloWorldAI/HellowWorldDemo.java
@@ -1,11 +1,17 @@
 package HelloWorldGI.HelloWorldAI;
 
 public class HellowWorldDemo {
+	
+	public static void testMethod() {
+		System.out.println("THIS IS HellowWorldDemo testMethod ");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("THIS IS HelloWorldDemo MAVEN EXAMPLE");
 		System.out.println("ANOTHER LINE ADDED");
+		testMethod();
+		
 
 	}
 
